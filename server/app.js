17e250f8cf8a3ffbde674736b74b3646d478772f
Dhairya Rathore <rathoredhairya2004@gmail.com>
2026-04-03 11:06:36 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -46,13 +46,7 @@ app.use(cookieParser());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST"],
-  },
-  transports: ["websocket", "polling"],
-  allowEIO3: true,
+  cors: corsConfig,
 });
 
 io.use(wrapSocketIo(cookieParser()));
